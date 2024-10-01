d09b00f5cdfe65f0839514b27a787cca21686acc
BrunoP030 <bpseisdedos@gmail.com>
2024-10-01 16:59:56 -0300

    Initial Commit

---
+++ src/main/java/com/example/aplicaFinanceiro/AplicaFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicaFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicaFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicaFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aplicaFinanceiro/AplicaFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aplicaFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicaFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
